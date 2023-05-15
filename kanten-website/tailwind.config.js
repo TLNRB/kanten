1294792085657b1fd10f5155899830ee1ff0a3f0
TLNRB <nortol01@easv365.dk>
2023-05-15 12:05:18 +0200

    fontAwsome with screen size and color variables and AdobeFonts added

--- kanten-website/tailwind.config.js
+++ kanten-website/tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    screens: {
+      xs: '360px',
+      sm: '560px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1440px'
+    },
+    fontFamily: {
+      'sans-serif': 'futura-pt',
+      'sans-serif': 'acumin-pro-wide'
+    },
+    extend: {
+      colors: {
+        darkBG: '#181818',
+        lightText: '#F4F4F4',
+        baseColor: '#ADFF00',
+        hipHopColor: '#81F4D1',
+        raveColor: '#A809E5',
+        houseColor: '#FF0000',
+        dancehallColor: '#FEFE00'
+      }
+    }
   },
   plugins: []
 }
