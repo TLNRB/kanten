af9f20582fa606aaa1f28e889e23f56d96809f60
Yevhenii <yevmot01@easv365.dk>
2023-05-23 16:29:50 +0200

    23.05

--- kanten-website/src/views/AllEventsView.vue
+++ kanten-website/src/views/AllEventsView.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+/* ---------- Importing Section Components ---------- */
+import Event from '../components/EventView/Event.vue'
+import Items from '../components/EventView/Items.vue';
+/* ---------- Importing Images ---------- */
+import event1 from '../images/event1.png'
+import event2 from '../images/event2.png'
+import event3 from '../images/event3.png'
+import event4 from '../images/event4.png'
+</script>
 
 <template>
-  <div>All Events</div>
+  <Event />
+  <Items :event1="event1" :event2="event2" :event3="event3" :event4="event4" /> 
+
 </template>
 
 <style scoped></style>
