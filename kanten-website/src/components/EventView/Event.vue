bdb63ce20f3379630e51ef6dd80dca2b1645823d
TLNRB <nortol01@easv365.dk>
2023-05-30 12:28:47 +0200

    all events updated

--- kanten-website/src/components/EventView/Event.vue
+++ kanten-website/src/components/EventView/Event.vue
@@ -1,61 +1,58 @@
 <script setup></script>
 
 <template>
-   <main
-    class="mt-[6.875rem] pt-[2rem] pb-[0.5rem] px-[1rem] overflow-x-hidden md:mt-[7.875rem] md:px-[4rem] lg:pt-[8rem] lg:pb-[2rem] xl:mt-[8.375rem] xxl:px-[12.5rem]"
-    >
+  <main
+    class="mt-[6.875rem] pt-[4rem] pb-[2rem] px-[1rem] overflow-x-hidden md:mt-[7.875rem] md:px-[4rem] lg:pt-[6rem] lg:pb-[4rem] xl:mt-[8.375rem] xxl:px-[12.5rem]"
+  >
     <div
-        class="mx-auto text-center flex flex-col  text-[2.5rem] xs:text-[3rem] sm:text-[4rem] xl:text-[5rem] xxxl:text-[6rem]"
-        >
-        <div class="flex flex-col items-center w-fit  mx-auto sm:flex-row sm:gap-[1rem] xs:flex-row xs:gap-[1rem]">
-  
-        <p
-            class="font-semibold text-[1.25rem] leading-snug xs:text-[1.5rem] sm:text-[2rem] sm:translate-y-[.5rem] xxl:text-[2.5rem]"
-          >
-            The 
-          </p>
-
-          <p class="flex flex-col relative uppercase leading-none sm:leading-none">
-            <span class="font-bold text-lightText z-[1]">Upcoming</span>
-            <span class="textStroke">Upcoming</span>
-          </p>
-          
-        </div>
-
-        <div class="flex flex-col items-center w-fit mx-auto sm:flex-row sm:gap-[1rem] xs:flex-row xs:gap-[1rem]">
-
-          <p
-            class="flex flex-col relative w-fit mx-auto leading-snug translate-y-[-12px] uppercase sm:leading-none sm:translate-y-0"
-          >
-            <span class="font-bold text-lightText z-[1]">Events</span>
-            <span class="textStroke">Events</span>
-          </p>
-
-          <p
-            class="font-semibold text-[1.25rem] leading-snug xs:text-[1.5rem] sm:text-[2rem] xxl:text-[2.5rem]"
-          >
-           at Kanten
-          </p>
-
-        </div>
-  </div>
+      class="mx-auto text-center flex flex-col text-[2.5rem] xs:text-[3rem] sm:text-[4rem] xl:text-[5rem] xxxl:text-[6rem]"
+    >
       <div
-      class=" py-[1rem]  xs:text-[1rem] xs:py-[0.5rem] sm:text-[1.25rem] sm:py-[1rem] xl:text-[1.75rem] xxxl:text-[2rem] "
-        >Mark your calendars and prepare for unforgettable nights filled with non-stop music, wild energy, and incredible vibes. Don’t miss out on the hottest events in town – join us at Kanten!
+        class="flex flex-col items-center w-fit mx-auto xs:flex-row xs:gap-[.75rem] sm:flex-row sm:gap-[1rem]"
+      >
+        <p
+          class="font-semibold text-[1.25rem] translate-y-[-.25rem] leading-none xs:text-[1.5rem] sm:text-[2rem] xxl:text-[2.5rem]"
+        >
+          The
+        </p>
+
+        <p class="flex flex-col relative uppercase leading-none sm:leading-none">
+          <span class="font-bold text-lightText z-[1]">Upcoming</span>
+          <span
+            class="absolute top-[2px] left-[2px] font-bold text-baseColor w-max md:left-[3px] md:top-[3px]"
+            >Upcoming</span
+          >
+        </p>
       </div>
-  
+
+      <div
+        class="flex flex-col items-center w-fit mx-auto xs:flex-row xs:gap-[.75rem] sm:flex-row sm:gap-[1rem]"
+      >
+        <p
+          class="flex flex-col relative w-fit mx-auto leading-snug translate-y-[-12px] uppercase sm:leading-none sm:translate-y-0"
+        >
+          <span class="font-bold text-lightText z-[1]">Events</span>
+          <span
+            class="absolute top-[2px] left-[2px] font-bold text-baseColor w-max md:left-[3px] md:top-[3px]"
+            >Events</span
+          >
+        </p>
+
+        <p
+          class="font-semibold text-[1.25rem] translate-y-[-1rem] leading-none xs:text-[1.5rem] xs:translate-y-[-.75rem] sm:text-[2rem] sm:translate-y-[-.25rem] xxl:text-[2.5rem]"
+        >
+          at Kanten
+        </p>
+      </div>
+    </div>
+    <div
+      class="pt-[1rem] xs:text-[1rem] sm:text-[1.25rem] sm:py-[1rem] leading-snug xl:text-[1.5rem]"
+    >
+      Mark your calendars and prepare for unforgettable nights filled with non-stop music, wild
+      energy, and incredible vibes. Don’t miss out on the hottest events in town – join us at
+      Kanten!
+    </div>
   </main>
-  
 </template>
 
-<style scoped>
-.textStroke {
-  /* color: #f4f4f4; */
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  width: max-content;
-  font-weight: bold;
-  color: #1ECECE;
-}
-</style>
+<style scoped></style>
--- kanten-website/src/views/AllEventsView.vue
+++ kanten-website/src/views/AllEventsView.vue
@@ -1,103 +1,109 @@
 <script setup>
-import { ref } from 'vue'
- 
-import singleevent from '../data/singleevent'
+import { ref, onMounted, onUnmounted, computed } from 'vue'
 /* ---------- Importing Section Components ---------- */
 import Event from '../components/EventView/Event.vue'
+import singleevent from '../data/singleevent'
 /* ---------- Importing Images ---------- */
+const { state } = singleevent()
 
-const {state} = singleevent()
+let numLines = ref(0)
 
+const backgroundLines = () => {
+  /* const lines = [] */
+  console.log(numLines.value)
+  if (state.value.length % 2 == 0) {
+    numLines.value = state.value.length / 2
+  } else {
+    numLines.value = Math.floor(state.value.length / 2)
+    numLines.value++
+  }
+
+  const screenWidth = window.innerWidth
+
+  // Calculate the number of lines based on screen width and number of cards
+  if (screenWidth < 1082) {
+    // Small screens
+    return numLines.value
+  } else {
+    // Large screens
+    if (state.value.length % 6 == 0) {
+      numLines.value = state.value.length / 6
+      return numLines.value
+    } else {
+      numLines.value = Math.floor(state.value.length / 6)
+      numLines.value++
+      return numLines.value
+    }
+  }
+}
+
+// Update the number of lines on window resize
+const updateBackgroundLines = () => {
+  backgroundLines() // Trigger re-computation of background lines
+}
+
+onMounted(() => {
+  backgroundLines()
+  window.addEventListener('resize', updateBackgroundLines)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateBackgroundLines)
+})
 </script>
 
 <template>
+  <div class="relative">
+    <Event />
 
-  <Event />
-
-  <section 
-     class="pb-[0.5rem] px-[1rem] md:px-[4rem] xxl:px-[12.5rem]">
-       <div 
-         class="flex flex-col flex-wrap	 sm:flex-row  justify-between">
-          <div
-              v-for="item in state" 
-              :key="item.id"
-              class="flex mb-[3rem] xs:w-[100%] xs:justify-center sm:w-[47%] lg:w-[31%]   flex-col p-2 bg-darkBG border-solid border-[1px] z-[1] border-baseColor">
-               <img class="object-cover h-[18rem] "
-               :src="item.image"
-                /> 
-               <h1 
-               class=" ml-[0.25rem] font-bold text-[1.75rem] z-[1] relative top-[-3rem] xs:text-[2rem] sm:text-[1.4rem] md:text-[1.75rem] xl:text-[2rem]">
-               {{ item.title }}
-               </h1>
-              <h3 
-               class=" ml-[0.25rem] relative top-[-3rem] text-[1.75rem] xs:text-[2.25rem] sm:text-[1.5rem] md:text-[1.75rem] xl:text-[2rem]">
-               {{ item.date }}
-              </h3>
-              <div class="ml-[0.25rem] group h-[0.9rem] xs:h-[1.8rem] sm:h-[0.5rem] md:h-[1rem] xl:h-[1.3rem]">
-                 <div 
-                 class="bg-darkBG relative z-[1] top-[-2rem] w-fit px-[1rem] py-[0.1rem] border-solid border-[1px] border-baseColor group-hover:text-baseColor group-hover:border-lightText ease-in duration-[.15s]">
-                    <RouterLink :to="{ name:'singleeventview', params:  { id:item.id}}" >
-                       <h3 
-                       class=" bg-darkBG text-[1.25rem] xs:text-[1.75rem] sm:text-[1rem] md:text-[1.25rem] xl:text-[1.5rem] ">
-                       Read more
-                      </h3>  
-                   </RouterLink>          
-                 </div>
-                <div
-                 class="borderStroke group-hover:mt-[0] sm:group-hover:mt-[-1px] md:group-hover:mt-[-2.5px] xl:group-hover:mt-[-1.9px] group-hover:ml-[0] duration-[.2s]">
-                 <h3 
-                 class=" text-[1.25rem] xs:text-[1.75rem] sm:text-[1rem] md:text-[1.25rem] xl:text-[1.5rem]">
-                 Read more
-              </h3>           
-            </div>
+    <section class="pb-[4rem] px-[1rem] md:px-[4rem] lg:pb-[6rem] xxl:px-[12.5rem]">
+      <div
+        class="flex flex-col justify-center items-center flex-wrap gap-[2.5rem] sm:gap-[4rem] sm:flex-row"
+      >
+        <div
+          v-for="item in state"
+          :key="item.id"
+          class="h-[416px] w-[248px] flex flex-col items-center p-[.5rem] bg-darkBG border-solid border-[1px] z-[1] border-baseColor xs:h-[434px] xs:w-[320px] sm:h-[580px] sm:w-[445px]"
+        >
+          <img class="object-cover h-[260px] w-[100%] sm:h-[348px]" :src="item.image" />
+          <div class="ml-[.25rem] flex flex-col justify-center">
+            <h1
+              class="font-bold leading-none text-[1.25rem] z-[1] relative top-[-.5rem] drop-shadow-md xs:text-[1.75rem] xs:top-[-.75rem] sm:text-[2.25rem] sm:top-[-1rem] sm:drop-shadow-xl"
+            >
+              {{ item.title }}
+            </h1>
+            <h3
+              class="relative font-[500] text-[1.25rem] leading-tight mt-[.5rem] xs:text-[1.5rem] xs:mt-[.25rem] sm:text-[2rem]"
+            >
+              {{ item.date }}
+            </h3>
+            <RouterLink
+              :to="{ name: 'singleeventview', params: { id: item.id } }"
+              class="flex flex-col w-fit mt-[1rem] mr-auto text-[1rem] relative group sm:mt-[1.5rem] sm:mb-[1.25rem]"
+            >
+              <span
+                class="font-[500] py-[.375rem] px-[1.125rem] border-[1px] bg-darkBG border-baseColor z-[1] group-hover:text-baseColor group-hover:border-lightText ease-in duration-[.15s] delay-[.05s] sm:py-[.5rem] sm:px-[1.25rem] sm:text-[1.5rem]"
+                >Read More</span
+              >
+              <span
+                class="font-[500] py-[.375rem] px-[1.125rem] border-[1px] border-lightText absolute top-[4px] right-[-4px] group-hover:top-[0] group-hover:right-[0] ease-in duration-[.2s] sm:py-[.5rem] sm:px-[1.25rem] sm:text-[1.5rem]"
+                >Read More</span
+              >
+            </RouterLink>
+          </div>
         </div>
-    </div>
-       </div>
-   </section>
-   <section class="h-[0rem]">    
-    <div class="bg-baseColor relative top-[-50rem] sm:top-[-40rem] md:top-[-45rem] h-[25rem]"></div>
-  </section>
-  <section class="h-[0rem]">    
-    <div class="bg-baseColor relative top-[-110rem] xs:top-[-130rem] sm:hidden h-[25rem]"></div>
-  </section>
-
-
+      </div>
+    </section>
+    <section
+      class="absolute top-[587px] bottom-[272px] left-0 right-0 flex flex-col items-center gap-[456px] z-[-1] xs:top-[553px] xs:bottom-[282px] xs:gap-[474px] sm:top-[656px] sm:bottom-[354px] sm:gap-[644px] lg:top-[665px] lg:bottom-[386px]"
+    >
+      <div
+        v-for="(line, index) in numLines"
+        :key="index"
+        class="bg-[url('../images/squareGrid.svg')] w-[100%] h-[456px] bg-baseColor xs:h-[474px] sm:h-[644px]"
+      ></div>
+    </section>
+  </div>
 </template>
 
-<style scoped>
-.borderStroke {
-  /* color: #f4f4f4; */
-  margin-top: 4px;
-  margin-left: 4px;
-  border: 1px solid #F4F4F4;
-  top: -4.55rem;
-  position: relative;
-  width: fit-content;
-  padding: 0.1rem 1rem;
-}
-@media screen and (max-width: 360px) {
-    .borderStroke{
-        top: -4.15rem;
-    }
-}
-@media screen and (min-width: 360px) {
-    .borderStroke{
-        top: -4.9rem;
-    }
-}
-@media screen and (min-width: 560px) {
-    .borderStroke{
-        top: -3.8rem;
-    }
-}
-@media screen and (min-width: 768px) {
-    .borderStroke{
-        top: -4rem;
-    }
-}
-@media screen and (min-width: 1200px) {
-    .borderStroke{
-        top: -4.5rem;
-    }
-}
-</style>
+<style scoped></style>
